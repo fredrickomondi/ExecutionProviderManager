c95dc6373dfe76ae5a47409216e08a8a39e43997
fredrickomondi <omondifredrick@gmail.com>
2023-05-11 10:28:40 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -10,5 +10,4 @@ if platform.system() == "Windows":
 model = "my_model.onnx"
 ep_manager = ExecutionProviderManager(model, "GPU_FP16") # assumes GPU is the inference device, for more device flags check https://onnxruntime.ai/docs/execution-providers/OpenVINO-ExecutionProvider.html
 inference_session = ep_manager.get_inference_session()
-print("Done")
 #inference_session.run()
